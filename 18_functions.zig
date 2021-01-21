a361b90a7562bcc5a62f863b42ae6a33aaa4b812
Dave Gauer <dave@ratfactor.com>
2021-01-21 17:23:09 -0500

    Hinting return type rather than making 'em guess.

--- 18_functions.zig
+++ 18_functions.zig
@@ -5,7 +5,7 @@ const std = @import("std");
 
 pub fn main() void {
     // The new function deepThought() should return the number 42. See below.
-    const answer = deepThought();
+    const answer: u8 = deepThought();
     
     std.debug.print("Answer to the Ultimate Question: {}\n", .{answer});
 }
