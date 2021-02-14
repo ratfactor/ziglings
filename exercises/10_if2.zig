e92107f5fcbf42e061f46bb6f1334fd66d65df09
Dave Gauer <dave@ratfactor.com>
2021-02-13 20:35:07 -0500

    ex10 - missing type was confusing and tangential (#12)

--- exercises/10_if2.zig
+++ exercises/10_if2.zig
@@ -10,7 +10,7 @@ pub fn main() void {
 
     // Please use an if...else expression to set "price".
     // If discount is true, the price should be $17, otherwise $20:
-    var price = if ???;
+    var price: u8 = if ???;
 
     std.debug.print("With the discount, the price is ${}.\n", .{price});
 }
