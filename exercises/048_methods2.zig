720a8aaac48d3aace15e41ada967cb82a901c85f
Dave Gauer <dave@ratfactor.com>
2021-03-16 11:00:12 -0400

    Enum submissions now closed in ex. 048

--- exercises/048_methods2.zig
+++ exercises/048_methods2.zig
@@ -61,17 +61,15 @@ fn visitElephants(first_elephant: *Elephant) void {
     }
 }
 
-// Bonus: Zig's enums can also have methods! Can you find
-// one in the wild? If you can, mention it along with your
-// name or alias in a comment below this one and make a
-// pull request on GitHub for a piece of eternal Ziglings
-// glory. The first five (5) PRs will be accepted!
+// Zig's enums can also have methods! This comment originally asked
+// if anyone could find instances of enum methods in the wild. The
+// first five pull requests were accepted and here they are:
 //
 // 1) drforester - I found one in the Zig source:
 // https://github.com/ziglang/zig/blob/041212a41cfaf029dc3eb9740467b721c76f406c/src/Compilation.zig#L2495
 //
 // 2) bbuccianti - I found one!
-// https://github.com/ziglang/zig/blob/master/lib/std/debug.zig#L477
+// https://github.com/ziglang/zig/blob/6787f163eb6db2b8b89c2ea6cb51d63606487e12/lib/std/debug.zig#L477
 //
 // 3) GoldsteinE - Found many, here's one
 // https://github.com/ziglang/zig/blob/ce14bc7176f9e441064ffdde2d85e35fd78977f2/lib/std/target.zig#L65
