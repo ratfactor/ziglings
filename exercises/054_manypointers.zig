ca70811f07853cc4ea0b027d9196c8cc74731214
Dave Gauer <dave@ratfactor.com>
2021-04-21 20:35:19 -0400

    Removed illegal type in pointer cheatsheet

--- exercises/054_manypointers.zig
+++ exercises/054_manypointers.zig
@@ -46,7 +46,6 @@ pub fn main() void {
 //   |  *u8          |  pointer to one u8                           |
 //   |  [2]u8        |  two u8s                                     |
 //   |  [*]u8        |  pointer to unknown number of u8s            |
-//   |  [2]const u8  |  two immutable u8s                           |
 //   |  [*]const u8  |  pointer to unknown number of immutable u8s  |
 //   |  *[2]u8       |  pointer to an array of 2 u8s                |
 //   |  *const [2]u8 |  pointer to an immutable array of 2 u8s      |
