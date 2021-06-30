88c550062d6da8869974e13e037e303bdaaff857
Dave Gauer <dave@ratfactor.com>
2021-06-30 19:55:47 -0400

    no unusued vars

--- exercises/084_async.zig
+++ exercises/084_async.zig
@@ -46,6 +46,8 @@
 const print = @import("std").debug.print;
 
 pub fn main() void {
+    // Additional Hint: you can assign things to '_' when you
+    // don't intend to do anything with them.
     foo();
 }
 
