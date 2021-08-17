a900877e402df5514c5b0550fc0c0793a0a22b0a
Palm <87849942+Palm404@users.noreply.github.com>
2021-08-17 00:01:55 -0700

    fix: Change order to match 011_while.zig

--- exercises/009_if.zig
+++ exercises/009_if.zig
@@ -10,8 +10,8 @@
 // Zig has the "usual" comparison operators such as:
 //
 //     a == b   means "a equals b"
-//     a > b    means "a is greater than b"
 //     a < b    means "a is less than b"
+//     a > b    means "a is greater than b"
 //     a != b   means "a does not equal b"
 //
 // The important thing about Zig's "if" is that it *only* accepts
