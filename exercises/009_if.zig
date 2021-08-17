57186aa56d1b1006377790357ba112b28149faff
unknown <palmbeach9917@gmail.com>
2021-08-16 23:56:10 -0700

    feat: Add more comparison operator coverage in 009_if.zig

--- exercises/009_if.zig
+++ exercises/009_if.zig
@@ -10,6 +10,7 @@
 // Zig has the "usual" comparison operators such as:
 //
 //     a == b   means "a equals b"
+//     a > b    means "a is greater than b"
 //     a < b    means "a is less than b"
 //     a != b   means "a does not equal b"
 //
