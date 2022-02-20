2699a3c21d5ee46c7a46113cd0c864b90e8a539a
InKryption <59504965+InKryption@users.noreply.github.com>
2022-02-20 19:24:36 +0100

    Update information to be accurate to the semantics of string literals

--- exercises/006_strings.zig
+++ exercises/006_strings.zig
@@ -9,7 +9,7 @@
 //
 // Is the same as:
 //
-//     const foo = [_]u8{ 'H', 'e', 'l', 'l', 'o' };
+//     const foo = &[_:0]u8{ 'H', 'e', 'l', 'l', 'o' };
 //
 // Notice how individual characters use single quotes ('H') and
 // strings use double quotes ("H"). These are not interchangeable!
