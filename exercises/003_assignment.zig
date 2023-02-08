be88d56387ff27f3d4bbdd3d1a61d4454964887b
Loden <loden.gendun@students.mq.edu.au>
2023-02-08 19:48:09 +1100

    solved exercises 001-008 🎉

--- exercises/003_assignment.zig
+++ exercises/003_assignment.zig
@@ -34,12 +34,12 @@
 const std = @import("std");
 
 pub fn main() void {
-    const n: u8 = 50;
+    var n: u8 = 50;
     n = n + 5;
 
-    const pi: u8 = 314159;
+    const pi: u32 = 314159;
 
-    const negative_eleven: u8 = -11;
+    const negative_eleven: i8 = -11;
 
     // There are no errors in the next line, just explanation:
     // Perhaps you noticed before that the print function takes two
