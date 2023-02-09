e61be6801e0ed9a77848f361b6f8e4af1a78f5cd
Loden <loden.gendun@students.mq.edu.au>
2023-02-09 11:20:54 +1100

    solved exercises 009-017 🎉

--- exercises/011_while.zig
+++ exercises/011_while.zig
@@ -21,7 +21,7 @@ pub fn main() void {
     var n: u32 = 2;
 
     // Please use a condition that is true UNTIL "n" reaches 1024:
-    while (???) {
+    while (n < 1024) {
         // Print the current number
         std.debug.print("{} ", .{n});
 
