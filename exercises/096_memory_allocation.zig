d608f481e6daf6600ddacb9509a574c02bc6eaf9
Chris Boesch <48591413+chrboesch@users.noreply.github.com>
2023-04-12 17:14:18 +0200

    Update 096_memory_allocation.zig
    formatting

--- exercises/096_memory_allocation.zig
+++ exercises/096_memory_allocation.zig
@@ -1,3 +1,4 @@
+//
 // In most of the examples so far, the inputs are known at compile
 // time, thus the amount of memory used by the program is fixed.
 // However, if responding to input whose size is not known at compile
